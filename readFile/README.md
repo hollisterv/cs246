c2028365d74d9de5e7466b267fc2a5865c6c9e73
Holly VanAuker <holly.vanauker@gmail.com>
2020-10-03 22:06:37 -0600

    revised program added new create file option

--- readFile/README.md
+++ readFile/README.md
@@ -1,10 +1,13 @@
+# Overview
+The point of this program is to read and write to new or existing files. 
+
 # Development Environment
 * visual Studio Code
 * Python 3.8.5
 
 # Execution
 To execute the program `python hello.py`
-![Program that records a list of grocery items](readingTheFile.PNG)
+![Program opens and closes files](files.PNG)
 
 # Useful Websites
 * W3Schools was a useful tool for me for this program
--- readFile/readFile.py
+++ readFile/readFile.py
@@ -1,16 +1,51 @@
-fileName = input("please enter a file name: ")
-answer = input("type r to read the file and type a to add new text: ")
+import time
 
+fileName = input("please enter a file name: ")
+
+#adding in snippets of time because i feel that it makes the program run at a more human eye pace.
+time.sleep(0.5)
+answer = input("type r to read the file | type a to add new text | type x to make a new file: ")
+
+#simply reading what is on the file
 if answer == "r":
     f = open(fileName, "r")
     print(f.read())
     f.close()
-else:
+
+#writing new things to the file
+elif answer == "a":
     f = open(fileName, "a")
-    print("Add to your grocery list: ")
+    time.sleep(0.5)
+    print("Add to your file: ")
     f.write(input())
     f.close
-    print("Here is your updated grocery list: ")
+    time.sleep(0.5)
+    print("Updated file: ")
+
     #opening again to read new content
     f = open(fileName, "r")
     print(f.read())
+
+#this section is meant to write new files to the program that the program will be
+#able to access later.
+else: 
+    time.sleep(0.5)
+    newFile = input("Enter a new file name: ")
+    f = open(newFile, "x")
+    time.sleep(0.5)
+    f.write(input())
+    f.close
+
+    #adding nested if statement to see if user wants to review what was written.
+    time.sleep(0.5)
+    readBack = input("type y if you want to review what you wrote | type n to finish").lower()
+    if readBack == 'y':
+        time.sleep(0.5)
+        print("Here is what you wrote in the new file: ")
+        f = open(newFile, "r")
+        print(f.read())
+
+        #closes file and lets the user know that writing is complete.
+    else:
+        f.close
+        print("Closing file...")
