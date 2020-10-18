067d947d0681d1f673fb131cfd73ea5eff6a582f
Holly VanAuker <holly.vanauker@gmail.com>
2020-10-17 21:02:52 -0600

    final draft of module #2

---
+++ BirthdayDatabase/README.md
@@ -0,0 +1,39 @@
+# Overview
+This database was originally intended to make note of family members' birthdays and addresses. To keep their identity and personal information private, i have made fake information for the sake of practicing my new skills in PostgreSQL. 
+
+I have also added some stretch goals with joining two tables together, adding an aggregate function and adding dates and filtering by those dates. i have inlcuded pictures of the results of each new technique. 
+
+# Development Environent
+* visual Studio Code 
+* PostgreSQL 13.0
+
+# Execution
+To execute the program: 
+* Download SQL tools onto VS code
+* Create a new database connection
+* Download database file
+* Click the run database button at the top part of the screen. 
+
+the following two pictures display the outcomes of creating the table, and adding new information to the table. I have left out deleting a part of the table for redundancy. 
+
+![Initial table result](firstTableResult.PNG)
+
+Adding puppy birthday and address
+
+![Second table Result](secondTableResult.PNG)
+
+Below are some stretch challenges. the first one adds a second table to the database and joins those two tables together
+
+![joing table](tableJoin.PNG)
+
+the second picture uses one aggregate function, age(). I felt that this one was best fitting to the tables that I created. I was very pleased with this filter result. 
+
+![age function aggregate](ageAggregate.PNG)
+
+this final stretch challenge filters the birthdates from 1998 and on. I purposefully intended for 2 birthdates to display.
+
+![filter dates](filterDates.PNG)
+
+# Useful Websites
+* [Downloaded PostgreSQL language](https://www.postgresql.org/)
+* [where most of my readings came from](https://www.postgresqltutorial.com/)
--- BirthdayDatabase/birthday.sql
+++ BirthdayDatabase/birthday.sql
@@ -1,5 +1,6 @@
 --this should drop the table to make room to create new table
 DROP TABLE IF EXISTS birthdays;
+DROP TABLE IF EXISTS favorites;
 
 --creating table
 CREATE TABLE birthdays (
@@ -17,9 +18,26 @@ VALUES (1, '1967-03-15', 'OGD', '430 N Scottsdale Rd, Suite A112, Tempe, Arizona
         (4, '1990-12-21', '2GD', '717 Lancaster Dr NE, Salem, Oregon 97301'),
         (5, '1992-6-3', '2GN', '717 Lancaster Dr NE, Salem, Oregon 97301'),
         (6, '2016-7-13', 'Velociraptor', '717 Lancaster Dr NE, Salem, Oregon 97301'),
-        (7, '2018-11-20', 'T-rex', '717 Lancaster Dr NE, Salem, Oregon 97301'),
+        (7, '2017-11-20', 'T-rex', '717 Lancaster Dr NE, Salem, Oregon 97301'),
         (8, '1994-12-14', 'AF', '160 W 2nd S, Suite 102, Rexburg, Idaho 83440'),
         (9, '1996-09-17', 'hollister', '160 W 2nd S, Suite 102, Rexburg, Idaho 83440');
+
+--second table favorite color, dessert, animal
+CREATE TABLE favorites (
+    user_num serial,
+    name2 VARCHAR(225) NOT NULL,
+    color VARCHAR(225) NOT NULL,
+    dessert VARCHAR(225) NOT NULL,
+    animal VARCHAR(225) NOT NULL
+);
+
+--adding information to favorites TABLE
+INSERT INTO favorites (user_num, name2, color, dessert, animal)
+VALUES (1, 'OGD', 'Red', 'German Chocolate Cake', 'Gorilla'),
+        (2, 'OGM', 'Green', 'Key lime pie', 'Frogs'),
+        (3, 'Tiana', 'Teal', 'Chocolate heaven', 'Llamas'),
+        (4, 'AF', 'red', 'steak', 'dog'),
+        (5, 'hollister', 'blue', 'brownies', 'fluffy puppies');
 
 --adding primary key to user_id
 ALTER TABLE birthdays
@@ -54,3 +72,15 @@ SELECT user_id AS "Family Number",
         address
 FROM birthdays
 WHERE birth_day > '1998-10-11';
+
+--inner join on birthdays to favorites joining user_ids
+SELECT user_id,
+        name,
+        user_num,
+        name2 
+FROM birthdays
+INNER JOIN favorites ON name = name2;
+
+--selecting age from birthdays as aggregate function
+SELECT AGE(birth_day)
+FROM birthdays;
