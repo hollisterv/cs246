abd722c6268b0b47f0016bcf9c76200bda552fa8
Holly VanAuker <holly.vanauker@gmail.com>
2020-11-14 16:35:50 -0700

    adding quilt row for the first time

---
+++ quiltRowCalculator/quiltRow.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    //welcome statement describing purpose of program
+    val welcome1 = "Welcome to the Quilt block row calculator. Here you will be able to\n"
+    val welcome2 = "calculate how many blocks you want in a specific width row. We will be adding\n"
+    val welcome3 = "seam allowance to our calculations so you won't have to! Just follow the steps\n"
+    val welcome4 = "to ensure your perfect calculation.\n"
+    println(welcome1 + welcome2 + welcome3 + welcome4)
+
+    //asking for how many blocks in a row 
+    //and asks for how long you want the quilt
+    println("How wide (in inches) would you like the quilt?")
+    //adding in variable instead of user input because i dont know how to run it
+    //on the output section
+    var wide = 40
+    println(wide) //pretending that this is user input
+
+    //asking for how many squares in the row
+    println("How many squares would you like in the row?")
+    var squares = 10
+    println(squares)
+
+    //this part is the calculation
+}
